e669131f0cbf4853fabb42b3605a313a447fa915
Mathias Buus <mathiasbuus@gmail.com>
2017-02-01 15:18:38 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# dat-share
+
+Experimental quick-n-dirty file sharing tool on top of dat
+
+```
+npm install dat-share
+```
+
+## Usage
+
+One one computer
+
+```
+dat-share ./some-file
+```
+
+On another
+
+```
+dat-share <key-from-above> ./some-file
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+var progress = require('progress-string')
+var speedometer = require('speedometer')
+var hypercore = require('hypercore')
+var hyperdiscovery = require('hyperdiscovery')
+var ansi = require('ansi-diff-stream')
+var pretty = require('prettier-bytes')
+var speed = require('speedometer')
+var ram = require('random-access-memory')
+var raf = require('random-access-file')
+var pump = require('pump')
+var fs = require('fs')
+var net = require('net')
+var minimist = require('minimist')
+var path = require('path')
+
+var argv = minimist(process.argv.slice(2), {alias: {sleep: 's'}})
+var uploadSpeed = speedometer()
+var downloadSpeed = speedometer()
+var diff = ansi()
+
+diff.pipe(process.stdout)
+
+var src = argv._[0]
+var dest = argv._[1]
+var key = null
+var downloaded = 0
+var bar = null
+
+if (dest) {
+  key = src
+  src = null
+}
+
+var feed = hypercore(key, {indexing: !key}, function (name) {
+  if (name === 'data') return raf(src || dest)
+  if (argv.sleep) return raf(path.join(argv.sleep, 'sleep', name))
+  return ram()
+})
+
+feed.ready(function () {
+  for (var i = 0; i < feed.blocks; i++) {
+    if (feed.has(i)) downloaded++
+  }
+
+  if (src) {
+    console.log('Share this command:\ndat-share ' + feed.key.toString('hex') + ' ' + JSON.stringify(src) + '\n')
+    if (!feed.blocks) fs.createReadStream(src).pipe(feed.createWriteStream())
+  } else {
+    feed.get(0, function () {
+      if (feed.blocks === downloaded) { // WORKAROUND
+        log()
+        process.exit(0)
+      }
+      feed.download(function () {
+        log()
+        process.exit(0)
+      })
+    })
+  }
+
+  log()
+  setInterval(log, 1000)
+  hyperdiscovery(feed)
+})
+
+feed.on('upload', function (index, data) {
+  uploadSpeed(data.length)
+})
+
+feed.on('download', function (index, data) {
+  downloaded++
+  downloadSpeed(data.length)
+})
+
+function log () {
+  if (!feed.blocks) return diff.write('Connecting to swarm ...')
+  if (!bar) bar = progress({width: 50, total: feed.blocks, style: a => a + '>' })
+
+  if (src) {
+    diff.write('Uploading ' + pretty(uploadSpeed()) + '/s')
+  } else if (feed.blocks === downloaded) {
+    diff.write(
+      '\n[' + bar(downloaded) + ']\n\n' +
+      'Download completed.'
+    )
+  } else {
+    diff.write(
+      '\n[' + bar(downloaded) + ']\n\n' +
+      'Downloading ' + pretty(downloadSpeed()) + '/s\n' +
+      'Uploading ' + pretty(uploadSpeed()) + '/s'
+    )
+  }
+}
